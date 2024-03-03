5760a6d40484e1425cb12df4f6f6dbae6dc4bfe6
zahida-abbasi <145322592+zahida-abbasi@users.noreply.github.com>
2024-03-03 12:01:50 +0500

    Add files via upload

---
+++ 012/q12.ts
@@ -0,0 +1,16 @@
+/*Zahida
+Governor Initiative IT program Q1: Assignments
+Roll No: 00216014
+*/
+
+/*
+Greetings: Start with the array you used in Exercise 11, but instead of just
+ printing each person’s name, print a message to them. The text of each message 
+ should be the same, but each message should be personalized with the person’s name.
+*/
+
+let myFriend:string[] = ["fida","Anwar","Altaf","daniyal","ahmed","ishtiaque"]
+
+for( let myFriends of myFriend){
+console.log(`Good Morning ${myFriends} I hope you have fantastic day`)
+}
