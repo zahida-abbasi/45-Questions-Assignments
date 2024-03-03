fd91bf072180c305f921fa215e3aa329fa6dcb62
zahida-abbasi <145322592+zahida-abbasi@users.noreply.github.com>
2024-03-03 11:52:47 +0500

    Add files via upload

---
+++ 007/q7.js
@@ -0,0 +1,23 @@
+"use strict";
+/*Zahida
+Governor Initiative IT program Q1: Assignments
+Roll No: 00216014
+*/
+/*
+Number Eight: Write addition, subtraction, multiplication,
+ and division operations that each result in the number 8.
+ Be sure to enclose your operations in print statements to
+ see the results.
+*/
+// Addition
+let additionResult = 5 + 3;
+console.log(`Addition Result: ${additionResult}`);
+// Subtraction
+let subtractionResult = 10 - 2;
+console.log(`Subtraction Result: ${subtractionResult}`);
+// Multiplication
+let multiplicationResult = 4 * 2;
+console.log(`Multiplication Result: ${multiplicationResult}`);
+// Division
+let divisionResult = 16 / 2;
+console.log(`Division Result: ${divisionResult}`);
---
+++ 007/q7.ts
@@ -0,0 +1,28 @@
+/*Zahida
+Governor Initiative IT program Q1: Assignments
+Roll No: 00216014
+*/
+
+
+/*
+Number Eight: Write addition, subtraction, multiplication,
+ and division operations that each result in the number 8. 
+ Be sure to enclose your operations in print statements to 
+ see the results.
+*/
+
+// Addition
+let additionResult: number = 5 + 3;
+console.log(`Addition Result: ${additionResult}`);
+
+// Subtraction
+let subtractionResult: number = 10 - 2;
+console.log(`Subtraction Result: ${subtractionResult}`);
+
+// Multiplication
+let multiplicationResult: number = 4 * 2;
+console.log(`Multiplication Result: ${multiplicationResult}`);
+
+// Division
+let divisionResult: number = 16 / 2;
+console.log(`Division Result: ${divisionResult}`);
