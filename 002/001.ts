5552e0aa0241e31a987ed678599104f436ebbb62
zahida-abbasi <145322592+zahida-abbasi@users.noreply.github.com>
2024-03-03 11:50:33 +0500

    Add files via upload

---
+++ 002/001.ts
@@ -0,0 +1,12 @@
+/*Zahida
+Governor Initiative IT program Q1: Assignments
+Roll No: 00216014
+ */
+
+/*Q:02- Personal Message: Store a person’s name in a variable, 
+and print a message to that person. Your message should be
+simple, such as, “Hello Eric, would you like to learn some Python 
+today?” */
+
+let nameVariable:string="Hello Eric, would you like to learn some Python today"
+console.log(nameVariable);
