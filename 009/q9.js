78e7b5c7415defa0bc203bc7af3df6f4984ed0b4
zahida-abbasi <145322592+zahida-abbasi@users.noreply.github.com>
2024-03-03 11:53:25 +0500

    Add files via upload

---
+++ 009/q9.js
@@ -0,0 +1,16 @@
+"use strict";
+/*Zahida
+Governor Initiative IT program Q1: Assignments
+Roll No: 00216014
+*/
+/*
+Favorite Number: Store your favorite number in a variable. Then, using that variable,
+create a message that reveals your favorite number. Print that message.
+
+*/
+// Store  favorite number in a variable
+let favoriteNumber = 90;
+// Create a message revealing favorite number
+let message = `My favorite number is ${favoriteNumber}.`;
+// Print the message
+console.log(message);
---
+++ 009/q9.ts
@@ -0,0 +1,21 @@
+/*Zahida
+Governor Initiative IT program Q1: Assignments
+Roll No: 00216014
+*/
+
+
+
+/*
+Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+create a message that reveals your favorite number. Print that message.
+
+*/
+
+// Store  favorite number in a variable
+let favoriteNumber: number = 90;
+
+// Create a message revealing favorite number
+let message: string = `My favorite number is ${favoriteNumber}.`;
+
+// Print the message
+console.log(message);
