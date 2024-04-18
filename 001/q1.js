141f5b3ad9cb9d8e2c0eb309770b5eb642b6ae01
zahida-abbasi <145322592+zahida-abbasi@users.noreply.github.com>
2024-04-18 11:55:53 +0500

    Add files via upload

---
+++ 001/q1.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Zahida
+Governor Initiative IT program Q1: Assignments
+Roll No: 00216014
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+//Q.1 Print hello world in typescript.
+console.log("Hello World");
---
+++ 001/q1.ts
@@ -0,0 +1,8 @@
+/*Zahida
+Governor Initiative IT program Q1: Assignments
+Roll No: 00216014
+ */
+
+//Q.1 Print hello world in typescript.
+
+console.log("Hello World");
